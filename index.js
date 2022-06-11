936f89d4b4789b8c572490f2126c30595885e8d9
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-11 23:22:10 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers,string){
+    const match = drivers.filter((driver)=>{
+        return driver.toLowerCase()===string.toLowerCase();
+    })
+    return match
+}
+function fuzzyMatch(drivers,string){
+    const match = drivers.filter((driver)=>{
+        return driver.slice(0,2)===string.slice(0,2);
+    })
+    return match
+}
+function matchName(drivers,string){
+    const match = drivers.filter((driver)=>{
+        return driver.name===string;
+    })
+    return match
+}
